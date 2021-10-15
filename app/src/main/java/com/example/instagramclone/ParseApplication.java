dee2d9a8c68795181111f090c34b5ddcbcbb8ca3
Ethan Chen <chengox12@gmail.com>
2021-10-15 16:42:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("B5OrZ6lgxOaVI466eZeIsuCh9vpOpYSqJ0ffd8ef")
+                .clientKey("PZugnwRUrYrnALqPKWeUZgdB6PTJ3aC8W9AcGdpd")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
